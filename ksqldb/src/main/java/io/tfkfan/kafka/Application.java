7a31d578d1d267e66422ecca3fbac2f4b7890ceb
tfkfan <abaltserdev@outlook.com>
2026-05-06 17:31:59 +0300

    Updated

--- ksqldb/src/main/java/io/tfkfan/kafka/Application.java
+++ ksqldb/src/main/java/io/tfkfan/kafka/Application.java
@@ -5,7 +5,7 @@ import io.confluent.ksql.api.client.ClientOptions;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.ExecutionException;
-
+import static java.lang.StringTemplate.STR;
 @Slf4j
 public class Application {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
@@ -18,11 +18,12 @@ public class Application {
         final String intermediateTableStream = "PAYMENTS_AGGREGATION_INTERMEDIATE_STREAM";
         final String finalStream = "PAYMENTS_FINAL_STREAM";
 
-        final String SQL_1 = StringTemplate.STR."""
+        try (final Client client = Client.create(options)) {
+            client.executeStatement(STR."""
                 CREATE OR REPLACE STREAM \{inputStream} (userId STRING KEY, amount DOUBLE)
                 WITH (KAFKA_TOPIC='\{Topics.PAYMENTS}', VALUE_FORMAT='AVRO');
-                """;
-        final String SQL_2 = StringTemplate.STR."""
+                """).get();
+            client.executeStatement(STR."""
                 CREATE TABLE IF NOT EXISTS \{aggregationTable} AS
                 SELECT userId,
                 SUM(amount) as amount,
@@ -30,21 +31,16 @@ public class Application {
                 from_unixtime(WINDOWEND) as window_end,
                 from_unixtime(max(ROWTIME)) as window_emit
                 FROM \{inputStream} WINDOW TUMBLING (SIZE 7 DAYS) GROUP BY userId EMIT CHANGES;
-                """;
-        final String SQL_3 = StringTemplate.STR."""
+                """).get();
+            client.executeStatement(STR."""
                 CREATE OR REPLACE STREAM \{intermediateTableStream} (userId STRING KEY, amount DOUBLE)
                 WITH (KAFKA_TOPIC='\{aggregationTable}', VALUE_FORMAT='AVRO');
-                """;
-        final String SQL_4 = StringTemplate.STR."""
+                """).get();
+            client.executeStatement(STR."""
                 CREATE OR REPLACE STREAM \{finalStream}
                 WITH (KAFKA_TOPIC='\{Topics.SUMMARY_LAST_WEEK}', VALUE_FORMAT='KAFKA')
                 AS SELECT userId, amount FROM \{intermediateTableStream} PARTITION BY userId EMIT CHANGES;
-                """;
-        try (final Client client = Client.create(options)) {
-            client.executeStatement(SQL_1).get();
-            client.executeStatement(SQL_2).get();
-            client.executeStatement(SQL_3).get();
-            client.executeStatement(SQL_4).get();
+                """).get();
         }
     }
 }
